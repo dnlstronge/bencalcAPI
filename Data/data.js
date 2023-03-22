e0ab9cfd3b50ef271b32715f2f8e1218cb67a507
Dan Stronge <dnlstronge@gmail.com>
2023-03-22 13:54:52 +0000

    [x] - add data to pcelements

--- Data/data.js
+++ Data/data.js
@@ -1,6 +1,19 @@
 const pcElements = {
 
-
+    GPCsingle: 182.60,
+    GPCcouple: 278.70,
+    SDP: 69.40,
+    SDP2: 138.80,
+    CARE: 38.85,
+    CARE2: 77.70,
+    SCSingle: 14.48,
+    SCCouple: 16.20,
+    capitalLimit: 10000,
+    childpre: 66.85,
+    childpost: 56.35,
+    disabledChild: 30.58,
+    disabledChildHigh: 95.48,
+    capitaldisregard: 10000
 }
 
 const ucElements = {
