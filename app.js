dae0b4f9404c0c8b5dd29fceb25802c56adda621
Dan Stronge <dnlstronge@gmail.com>
2023-03-22 13:42:50 +0000

    [x] - add comments

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// needs to allow get request (can be public) for UC & PC elements
+// should send back complete object of key-value pairs which represent the different elements
+// needs hosted somewhere
+// needs package.json and dependencies
+
+
 const express = require("express")
 const app = express();
 const port = 3000
